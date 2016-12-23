9be7b0cb7921a7e00c79aa383e418ab96ec72499
wuanran <wuanran@anruence.local>
2016-12-23 18:14:35 +0800

    add links

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -34,6 +34,18 @@
                     ['label' => 'Blog', 'icon' => 'fa fa-file-code-o', 'url' => ['/blog']],
                     ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug']],
                     ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+                    ['label' => '分类管理', 'icon' => 'fa fa-th-list', 'url' => ['/category'],],
+                    ['label' => '用户管理', 'icon' => 'fa fa-users', 'url' => ['/user'],],
+                    [
+                        'label' => '一元夺宝',
+                        'icon' => 'fa fa-circle-o',
+                        'url' => '#',
+                        'items' => [
+                            ['label' => '商品管理', 'icon' => 'fa fa-circle-o', 'url' => '/oneproduct',],
+                            ['label' => '期数管理', 'icon' => 'fa fa-circle-o', 'url' => '/oneproterm',],
+                            ['label' => '抽奖管理', 'icon' => 'fa fa-circle-o', 'url' => '/onelottery',],
+                        ],
+                    ],
                     [
                         'label' => 'Same tools',
                         'icon' => 'fa fa-share',
@@ -41,6 +53,7 @@
                         'items' => [
                             ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii'],],
                             ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug'],],
+                            ['label' => 'Audit', 'icon' => 'fa fa-dashboard', 'url' => ['/audit'],],
                             [
                                 'label' => 'Level One',
                                 'icon' => 'fa fa-circle-o',
