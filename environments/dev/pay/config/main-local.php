69431325935b721405d6fa719e532c0a3cab7cd3
wuanran <wuanran@meituan.com>
2017-05-02 19:09:49 +0800

    open all ip switch

--- environments/dev/pay/config/main-local.php
+++ environments/dev/pay/config/main-local.php
@@ -14,10 +14,14 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*']
+
     ];
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['*']
+
     ];
 }
 
